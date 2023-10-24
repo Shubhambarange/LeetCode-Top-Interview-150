4473979d4afdb6aad59fe5d2f38a9eeaf43494e5
Shubham <95977286+Shubhambarange@users.noreply.github.com>
2023-10-24 13:15:07 +0530

    Create 12_Integer_to_Roman.py

---
+++ 12_Integer_to_Roman.py
@@ -0,0 +1,34 @@
+Example 1:
+
+Input: num = 3
+Output: "III"
+Explanation: 3 is represented as 3 ones.
+Example 2:
+
+Input: num = 58
+Output: "LVIII"
+Explanation: L = 50, V = 5, III = 3.
+Example 3:
+
+Input: num = 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        num_map = {
+            1: "I",
+            5: "V",    4: "IV",
+            10: "X",   9: "IX",
+            50: "L",   40: "XL",
+            100: "C",  90: "XC",
+            500: "D",  400: "CD",
+            1000: "M", 900: "CM",
+        }
+
+        res = ''
+        for n in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            while n<= num:
+                res = res + num_map[n]
+                num = num - n
+        return  res
