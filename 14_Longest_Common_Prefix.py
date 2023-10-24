698dbda5c8806eda7847edb4157eb4193bba11e2
Shubham <95977286+Shubhambarange@users.noreply.github.com>
2023-10-24 13:17:45 +0530

    Create 14_Longest_Common_Prefix.py

---
+++ 14_Longest_Common_Prefix.py
@@ -0,0 +1,20 @@
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+  
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res
