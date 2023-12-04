5faff5bb4617f65fdcb65174fcb76a8aa3eaf240
Shubham <95977286+Shubhambarange@users.noreply.github.com>
2023-12-04 12:18:51 +0530

    Create 909. Snakes and Ladders.py

---
+++ 909. Snakes and Ladders.py
@@ -0,0 +1,64 @@
+You are given an n x n integer matrix board where the cells are labeled from 1 to n2 in a Boustrophedon style starting from the bottom left of the board (i.e. board[n - 1][0]) and alternating direction each row.
+
+You start on square 1 of the board. In each move, starting from square curr, do the following:
+
+Choose a destination square next with a label in the range [curr + 1, min(curr + 6, n2)].
+This choice simulates the result of a standard 6-sided die roll: i.e., there are always at most 6 destinations, regardless of the size of the board.
+If next has a snake or ladder, you must move to the destination of that snake or ladder. Otherwise, you move to next.
+The game ends when you reach the square n2.
+A board square on row r and column c has a snake or ladder if board[r][c] != -1. The destination of that snake or ladder is board[r][c]. Squares 1 and n2 do not have a snake or ladder.
+
+Note that you only take a snake or ladder at most once per move. If the destination to a snake or ladder is the start of another snake or ladder, you do not follow the subsequent snake or ladder.
+
+For example, suppose the board is [[-1,4],[-1,3]], and on the first move, your destination square is 2. You follow the ladder to square 3, but do not follow the subsequent ladder to 4.
+Return the least number of moves required to reach the square n2. If it is not possible to reach the square, return -1.
+
+ Example 1:
+Input: board = [[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]]
+Output: 4
+Explanation: 
+In the beginning, you start at square 1 (at row 5, column 0).
+You decide to move to square 2 and must take the ladder to square 15.
+You then decide to move to square 17 and must take the snake to square 13.
+You then decide to move to square 14 and must take the ladder to square 35.
+You then decide to move to square 36, ending the game.
+This is the lowest possible number of moves to reach the last square, so return 4.
+
+class Solution:
+    def label2rc(self, label, n):
+        r = n - 1 - (label-1)//n
+        c = (label-1) % n
+        if ((n & 1) and (r & 1)) or ((n & 1 == 0) and (r & 1 == 0)):
+            c = n - 1 - c
+        return r,c
+         
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        n = len(board)
+        n2 = n * n
+        visited = [maxsize] * (n2 + 1)
+
+        visited[1] = 0
+
+        q = [1]
+        ret = maxsize
+
+        while q:
+            count = len(q)
+            for _ in range(count):
+                lbl = q.pop(0)
+                j = visited[lbl] + 1
+
+                start = lbl + 1
+                end = min(lbl+6, n2)
+                for l in range(start, end+1):
+                    r,c = self.label2rc(l, n)
+                    if board[r][c] != -1:
+                        l = board[r][c]
+                    if l == n2:
+                        ret = min(ret, j)
+                    if visited[l] > j:
+                        q.append(l)
+                        visited[l] = j
+
+        return ret if ret != maxsize else -1
+        
